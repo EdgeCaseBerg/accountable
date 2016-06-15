2ec5e584a73002549f427a6a9d620058e9badee4
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-06-15 00:47:46 -0400

    Remove ExpenseTag, Create ExpenseGroup
    I basically convinced myself in my last commit message that semantically
    I'd find it easier to work with an ExpenseGroup rather than with
    ExpenseTags at first. I think they're a more natural fit for BGI's
    "accounts".
    
    I think the next step will be to bootstrap a simple service for creating
    and reading Expenses. Then I'll create a service to create and read
    groups with. Then I'll update the services to be able to add expenses to
    groups. From there I'll be able to work on an import service.

--- app/models/domain/ExpenseTag.scala
+++
@@ -1,8 +0,0 @@
-package models.domain
-
-import java.util.UUID
-
-/** An expense tag is just a way to indicate that an expense is 'tagged' with
- *  a grouping label.
- */
-case class ExpenseTag(val name: String, val tagId: UUID = UUID.randomUUID())
