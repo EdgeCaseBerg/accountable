5c80782032576b8c0d57d35c3866c07ee2fd800f
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-06-15 01:25:12 -0400

    Formatting

--- app/models/domain/ExpenseGroup.scala
+++ app/models/domain/ExpenseGroup.scala
@@ -4,8 +4,8 @@ import java.util.UUID
 
 /** A label for grouping expenses together
  *
- * @note The relationship between an expense and a group will be Many to One
- * @param name A human friendly name to read the group as
- * @param groupId A machine friendly name to identify this group uniquely
+ *  @note The relationship between an expense and a group will be Many to One
+ *  @param name A human friendly name to read the group as
+ *  @param groupId A machine friendly name to identify this group uniquely
  */
 case class ExpenseGroup(val name: String, val groupId: UUID = UUID.randomUUID())
