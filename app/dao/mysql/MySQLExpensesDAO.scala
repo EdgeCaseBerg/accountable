392482bcf047deb9e336b4d82e2ea01c16e8333c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-06-20 19:36:08 -0400

    Add MySQLExpensesDAO (no impl)
    No implementation yet of the actual methods because I need to write the
    utils package first. Using the `???` format for now to avoid compilation
    errors from not having an implementation

---
+++ app/dao/mysql/MySQLExpensesDAO.scala
@@ -0,0 +1,24 @@
+package dao.mysql
+
+import dao._
+import models.domain._
+import scala.concurrent.Future
+import javax.inject.Inject
+import java.time.Instant
+
+class MySQLExpensesDAO @Inject() (mysqlConnector: MySQLConnector) extends ExpensesDAO {
+
+	/** @inheritdoc
+	 *
+	 */
+	def createNewExpense(expense: Expense): Future[Unit] = ???
+
+	/** @inheritdoc
+	 */
+	def listExpensesDuringWeekOf(epochInstant: Instant): Future[List[Expense]] = ???
+
+	/** @inheritdoc
+	 */
+	def listExpensesByGroupDuringWeekOf(epochInstant: Instant): Future[Map[ExpenseGroup, List[Expense]]] = ???
+
+}
