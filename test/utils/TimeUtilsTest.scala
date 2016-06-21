00c2aa5e0d2916d7c09204b8ddc9154c6b0cbd6f
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-06-20 22:30:57 -0400

    Fix test value in TimeUtilsTest
    Had a monday rather than a sunday by accident

--- test/utils/TimeUtilsTest.scala
+++ test/utils/TimeUtilsTest.scala
@@ -9,11 +9,18 @@ class TimeUtilsTest extends FlatSpec with Matchers {
 	val sunday12Am = 1466294400
 	val testCasesThatReturnSunday = (0 until 7).map(t => sunday12Am + 86400 * t)
 	val midDayOfEachTestCaseThatReturnsSunday = testCasesThatReturnSunday.map(_ + 86400 / 2)
-	val nextSunday12Am = 1466985600
+	val nextSunday12Am = 1466899200
+	val previousSunday = 1465689600
 
 	"getWeekOf" should "return the next sunday if the time given falls in it" in {
 		assertResult(nextSunday12Am.toInstant) {
 			TimeUtils.getWeekOf(sunday12Am.toInstant.plus(8, DAYS))
+		}
+	}
+
+	it should "return the previousSunday if the time given falls in it" in {
+		assertResult(previousSunday.toInstant) {
+			TimeUtils.getWeekOf(sunday12Am.toInstant.minus(2, DAYS))
 		}
 	}
 
