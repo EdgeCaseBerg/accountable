c3e0ed8916e2933bb8f502d9ec69e687696e961b
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-06-20 22:33:19 -0400

    Implement TimeUtils#getWeekOf
    All tests passing

--- app/utils/TimeUtils.scala
+++ app/utils/TimeUtils.scala
@@ -1,6 +1,6 @@
 package utils
 
-import java.time.Instant
+import java.time._
 
 /** A utility for dealing with time
  */
@@ -13,7 +13,15 @@ object TimeUtils {
 	 *  @return The Instant in time when the week began
 	 */
 	def getWeekOf(epochInstant: Instant): Instant = {
-		Instant.now()
+		val currentLocalDateTime: LocalDate = LocalDateTime.ofInstant(epochInstant, ZoneId.of("UTC")).toLocalDate()
+		val startOfTheWeek = currentLocalDateTime.getDayOfWeek match {
+			case DayOfWeek.SUNDAY => currentLocalDateTime.atStartOfDay(ZoneId.of("UTC"))
+			case _ => {
+				val daysToSubtract = ((7 + currentLocalDateTime.getDayOfWeek().getValue()) % 8) + 1
+				currentLocalDateTime.minusDays(daysToSubtract).atStartOfDay(ZoneId.of("UTC"))
+			}
+		}
+		Instant.from(startOfTheWeek)
 	}
 
 	/** Implicit to quickly convert a long into an Instant value
