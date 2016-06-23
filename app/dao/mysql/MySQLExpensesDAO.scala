db0e7466347cf632e6fae1f2a8af9328bb3ff349
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-06-22 22:38:50 -0400

    Start in on the MySQLExpensesDAO
    But just had a lot of trivia if you know what I mean, going to bed

--- app/dao/mysql/MySQLExpensesDAO.scala
+++ app/dao/mysql/MySQLExpensesDAO.scala
@@ -2,16 +2,32 @@ package dao.mysql
 
 import dao._
 import models.domain._
-import scala.concurrent.Future
+import scala.concurrent.{ Future, future, ExecutionContext }
 import javax.inject.Inject
 import java.time.Instant
+import anorm._
 
-class MySQLExpensesDAO @Inject() (mysqlConnector: MySQLConnector) extends ExpensesDAO {
+class MySQLExpensesDAO @Inject() (mysqlConnector: MySQLConnector)(implicit executionContext: ExecutionContext) extends ExpensesDAO {
 
 	/** @inheritdoc
 	 */
-	def createNewExpense(expense: Expense): Future[Unit] = ???
-
+	def createNewExpense(expense: Expense): Future[Unit] = {
+		val insertOperation = future {
+			mysqlConnector.withTransaction { implicit connection =>
+				val possibleNewId = SQL(
+					"""INSERT INTO expenses (amountInCents,name,dateOccured,expenseId) VALUES ({amountInCents}, {name}, {dateOccured}, expenseId)"""
+				).on(
+						"amountInCents" -> expense.amountInCents,
+						"name" -> expense.name,
+						"dateOccured" -> expense.dateOccured,
+						"expenseId" -> expense.expenseId
+					).executeInsert()
+				possibleNewId.fold(throw new RuntimeException("Could not save termId"))(identity)
+			}
+			()
+		}
+		insertOperation
+	}
 	/** @inheritdoc
 	 */
 	def listExpensesDuringWeekOf(epochInstant: Instant): Future[List[Expense]] = ???
