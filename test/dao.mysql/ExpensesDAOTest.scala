619077e054d6284b2a673b15e28cf06969915a80
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-06-27 23:45:06 -0400

    Add ExpensesDAOTest

---
+++ test/dao.mysql/ExpensesDAOTest.scala
@@ -0,0 +1,30 @@
+package utils
+
+import dao.mysql._
+import models.domain._
+
+import org.scalatest.{ FlatSpec, Matchers }
+import java.time.Instant
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ExpensesDAOTest extends testhelpers.MigratedAndCleanDatabase {
+
+	lazy val expensesDAO = new MySQLExpensesDAO(mySqlConnection)
+
+	val myTestExpense = Expense(amountInCents = 100, name = "Test Expense", dateOccured = 1467083533)
+
+	"The ExpensesDAO" should "create a new Expense" in {
+		whenReady(expensesDAO.createNewExpense(myTestExpense)) { unit =>
+			assert(true)
+		}
+	}
+
+	it should "return the created expense when listing the week it is from" in {
+		whenReady(expensesDAO.listExpensesDuringWeekOf(Instant.ofEpochSecond(myTestExpense.dateOccured))) { expensesList =>
+			expensesList.find(_.expenseId == myTestExpense.expenseId).fold(fail("Did not load saved expense")) { theLoadedExpense =>
+				assertResult(myTestExpense)(theLoadedExpense)
+			}
+		}
+	}
+
+}
