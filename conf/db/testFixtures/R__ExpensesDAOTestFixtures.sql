fb563f0d77340352716f9731725f250440e7bafd
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-07-14 00:09:31 -0400

    Update R__ExpensesDAOTestFixtures fixture
    Forgot to link the group and the expense together.

--- conf/db/testFixtures/R__ExpensesDAOTestFixtures.sql
+++ conf/db/testFixtures/R__ExpensesDAOTestFixtures.sql
@@ -1,3 +1,4 @@
 INSERT INTO expenseGroups (name, groupId) VALUES ("TestGroup","b95bd45d-f654-42fb-8cea-7c7e8db230e9");
 INSERT INTO expenses (amountInCents, name, dateOccured, expenseId) VALUES 
 (200, "My Fixture Expense", "2015/06/28", "07f256f0-f830-495d-b6cb-fe39bb5f2f36");
+INSERT INTO expenseGroupToExpense (groupId, expenseId) VALUES ("b95bd45d-f654-42fb-8cea-7c7e8db230e9", "07f256f0-f830-495d-b6cb-fe39bb5f2f36");
