7241923e0462da5754b19db5e7879b59363c2409
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-07-18 00:16:07 -0400

    Add Test fixtures for ExpenseGroups DAO testing

---
+++ conf/db/testFixtures/R__ExpenseGroupsDAOTestFixtures.sql
@@ -0,0 +1,4 @@
+INSERT INTO expenses (amountInCents, name, dateOccured, expenseId) VALUES 
+(200, "My Fixture Expense", "2016/06/28", "07f256f0-f830-495d-b6cb-fe39bb5f2f36");
+
+INSERT INTO expenseGroups (name, groupId) VALUES ("FixtureGroup","b95bd45d-f654-42fc-8cea-7c7e8db230e9");
