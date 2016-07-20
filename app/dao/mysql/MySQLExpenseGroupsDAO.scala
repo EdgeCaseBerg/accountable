f885c954e96095627c7daa88cee1369a4f07f4e3
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-07-20 05:11:08 -0400

    Add implementation of create expense group

--- app/dao/mysql/MySQLExpenseGroupsDAO.scala
+++ app/dao/mysql/MySQLExpenseGroupsDAO.scala
@@ -13,7 +13,27 @@ import anorm._
 class MySQLExpenseGroupsDAO @Inject() (mysqlConnector: MySQLConnector)(implicit executionContext: ExecutionContext) extends ExpenseGroupsDAO {
 
 	def addExpenseToGroup(expenseId: UUID, expenseGroup: ExpenseGroup): Future[Unit] = ???
-	def createExpenseGroup(expenseGroup: ExpenseGroup): Future[Unit] = ???
+
+	/** @inheritdoc
+	 */
+	def createExpenseGroup(expenseGroup: ExpenseGroup): Future[Unit] = {
+		val insertOperation = Future {
+			mysqlConnector.withTransaction { implicit connection =>
+				val numberOfInsertedRow = SQL(
+					"""INSERT INTO expenseGroups (name,groupId) VALUES ({name}, {groupId})"""
+				).on(
+						"name" -> expenseGroup.name,
+						"groupId" -> expenseGroup.groupId
+					).executeUpdate()
+				if (numberOfInsertedRow != 1) {
+					throw new RuntimeException(s"Could not create expenseGroup with id of ${expenseGroup.groupId}")
+				}
+			}
+			()
+		}
+		insertOperation
+	}
+
 	def listExpenseGroups(): Future[List[ExpenseGroup]] = ???
 
 }
