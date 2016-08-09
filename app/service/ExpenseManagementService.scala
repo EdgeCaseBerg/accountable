641cc3f0848b5cc3b3d6cae5fc34503909635a65
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-08-09 00:05:31 -0400

    Start in on ExpenseManagementService
    Not going to make two seperate services for the expense and expense
    group because it seems easy to get confused on remembering where a
    createExpeneAndAddToGroup method would live, and so I'll just put them
    all into one management service.
    
    The service layer will bulk up once I start adding in other pieces of
    funtionality, like the actual accountability part of the application and
    the email notifications.

---
+++ app/service/ExpenseManagementService.scala
@@ -0,0 +1,61 @@
+package service
+
+import dao._
+import dao.exceptions._
+import models.domain._
+
+import javax.inject.Inject
+import java.time.Instant
+import java.util.UUID
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global // For now, Change later
+
+/** Service to coordinate actions involving both ExpenseGroups and Expenses
+ *
+ *  @note The constructor to ExpenseManagementService is annotated with javax.inject.Inject for use by dependency injection frameworks.
+ *
+ *  @param expenseGroupsDAO An instance of a class implementing the ExpenseGroupsDAO trait
+ *  @param expensesDAO An instance of a class implementing the ExpensesDAO trait
+ */
+class ExpenseManagementService @Inject() (expenseGroupsDAO: ExpenseGroupsDAO, expensesDAO: ExpensesDAO) {
+
+	/** Creates and persists an ExpenseGroup to the database
+	 *
+	 *  @param name The name of the ExpenseGroup to create
+	 *
+	 *  @return The created ExpenseGroup in the result of a future, or an exception
+	 */
+	def createNewExpenseGroupWithName(name: String): Future[ExpenseGroup] = {
+		val expenseGroup = ExpenseGroup(name)
+		expenseGroupsDAO.createExpenseGroup(expenseGroup).map { _ =>
+			expenseGroup
+		}.recover {
+			case e: DAOException => throw e // No real backup plan, so just throw the exception
+		}
+	}
+
+	/** List all expense groups
+	 *
+	 *  @return A Future containing a list of ExpenseGroups
+	 */
+	def listExpenseGroups() = expenseGroupsDAO.listExpenseGroups()
+
+	/** Persist an Expense and link it to an ExpenseGroup
+	 *
+	 *  @param expense The Expense to persist to the underlying DAO
+	 *  @param expenseGroup The group to add the expense to once it's persisted
+	 */
+	def createExpenseAndAddToGroup(expense: Expense, expenseGroup: ExpenseGroup): Future[Unit] = {
+		expensesDAO.createNewExpense(expense).map { _ =>
+			expense
+		}.recover {
+			case e: DuplicateDataException => expense // If it's duplicate that's fine, just add it to the group
+		}.map { createdExpense =>
+			expenseGroupsDAO.addExpenseToGroup(expense.expenseId, expenseGroup)
+		}
+	}
+
+	def listCurrentWeeksCurrentExpenses(): Future[List[Expense]] = {
+		expensesDAO.listExpensesDuringWeekOf(Instant.now())
+	}
+}
