7a1efa4a315bdd3e3d2124548e84607f833ab0b4
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-08-24 19:33:35 -0400

    Add more tests for ExpenseManagementServiceTest
    Testing that listing the current week's expense works properly. I
    suppose these tests will probably fail if you run them at the cusp of
    midnight on a sunday and from start to finish we cross the day line, but
    that's OK I think.

--- test/service/ExpenseManagementServiceTest.scala
+++ test/service/ExpenseManagementServiceTest.scala
@@ -2,10 +2,21 @@ package service
 
 import testhelpers.ServiceTestWithDB
 import models.domain._
+import java.util.UUID
+
+import org.scalatest._
+import matchers.ShouldMatchers._
+import OptionValues._
 
 class ExpenseManagementServiceTest() extends ServiceTestWithDB {
 
 	var expenseGroup: Option[ExpenseGroup] = None
+
+	addSQLScriptForFlywayToLoad("conf/db/testFixtures/R__ExpenseManagementServiceTestFixtures.sql")
+
+	val fixtureExpenseId = UUID.fromString("07f256f0-f830-495d-b6cb-fe39bb5f2f36")
+
+	val fixtureExpenseGroupId = UUID.fromString("b95bd45d-f654-42fb-8cea-7c7e8db230e9")
 
 	"The ExpenseManagementService" should "make an expense group by name" in {
 		withExpenseManagementService { expenseManagementService =>
@@ -21,6 +32,25 @@ class ExpenseManagementServiceTest() extends ServiceTestWithDB {
 		withExpenseManagementService { expenseManagementService =>
 			whenReady(expenseManagementService.listExpenseGroups()) { groups =>
 				assert(groups.find(_.name == "Test Group").isDefined)
+			}
+		}
+	}
+
+	it should "list the current week's expense (loaded from fixture)" in {
+		withExpenseManagementService { expenseManagementService =>
+			whenReady(expenseManagementService.listCurrentWeeksCurrentExpenses()) { expenses =>
+				assertResult(fixtureExpenseId)(expenses.head.expenseId)
+			}
+		}
+	}
+
+	it should "list the current week's expense by group (loaded from fixture)" in {
+		withExpenseManagementService { expenseManagementService =>
+			whenReady(expenseManagementService.listCurrentWeeksCurrentExpensesWithGroup()) { expensesByGroup =>
+				val fixtureGroup = expensesByGroup.find { case (group, listOfExpenses) => group.groupId == fixtureExpenseGroupId }
+				val (group, listOfExpenses) = fixtureGroup.value
+				assertResult(fixtureExpenseGroupId)(group.groupId)
+				assertResult(fixtureExpenseId)(listOfExpenses.head.expenseId)
 			}
 		}
 	}
