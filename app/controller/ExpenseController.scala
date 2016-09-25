fb5f2c01038e625d61f872c4f8b42bb3bad6a782
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-09-25 17:57:23 -0400

    Flush out create expense action in ExpenseController
    Still need to actually create some viewss and such, but the basic
    functionality is encoded for creating an expense even if the way the
    final results, error or success, are shown to the user are currently
    sup-par.
    
    Need to skeleton an action for displaying a create expense form, and
    then I think I'll be good to work on that small part of the view.
    
    Once that's done then I can do the expense group's forms and whatnot.
    Which should be pretty easy.

--- app/controller/ExpenseController.scala
+++ app/controller/ExpenseController.scala
@@ -3,8 +3,10 @@ package controller
 import play.api.mvc._
 import javax.inject.Inject
 import scala.util.control.NonFatal
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ ExecutionContext, Future }
 import service._
+import models.domain._
+import forms.ExpenseForms
 
 class ExpenseController @Inject() (expenseManagementService: ExpenseManagementService, executionContext: ExecutionContext) extends Controller {
 	implicit val ec = executionContext
@@ -30,9 +32,35 @@ class ExpenseController @Inject() (expenseManagementService: ExpenseManagementSe
 		}
 	}
 
-	def createExpense = Action {
-		// TO-DO: Bind form, create expense
-		BadRequest("todo")
+	def createExpense = Action.async { implicit request =>
+		ExpenseForms.createExpenseForm.bindFromRequest.fold(
+			formWithErrors => {
+				Future.successful(BadRequest(formWithErrors.errors.mkString(",")))
+			},
+			boundForm => {
+				val (newExpense, maybeGroupId) = boundForm
+				maybeGroupId.fold {
+					expenseManagementService.createExpense(newExpense).recover {
+						case NonFatal(e) => BadRequest(e.getMessage())
+					}.map { createdExpense =>
+						Ok("created expense! " + createdExpense)
+						//To-do: Redirect() to create form if we're making another, or redirect back to the summary for the week of this expense
+					}
+				} { groupId =>
+					expenseManagementService.listExpenseGroups.recover {
+						case NonFatal(e) => List.empty[ExpenseGroup]
+					}.flatMap { groups =>
+						groups.find(_.groupId == groupId).fold(Future.successful(BadRequest(s"Group ${groupId} does not exist"))) { groupToAddTo =>
+							expenseManagementService.createExpenseAndAddToGroup(newExpense, groupToAddTo).recover {
+								case NonFatal(e) => BadRequest(e.getMessage())
+							}.map { _ =>
+								Ok("Added expense to group")
+							}
+						}
+					}
+				}
+			}
+		)
 	}
 
 }
