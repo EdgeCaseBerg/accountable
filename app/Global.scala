b910282f6ddf25cc0c40e02b1e8c6ca48ff18926
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-10-25 00:27:58 -0400

    Handle general errors in Global

--- app/Global.scala
+++ app/Global.scala
@@ -1,11 +1,14 @@
 /* No package because by default play will grab a Global from the default package */
 
 import modules._
+import models.view._
+import play.api.mvc.{ RequestHeader, Results }
 import dao.mysql.MySQLDatabaseParameters
 import com.google.inject.Guice
 import org.flywaydb.core.Flyway
 import play.api.{ GlobalSettings, Application, Logger }
 import scala.util.control.NonFatal
+import scala.concurrent.Future
 
 object Global extends GlobalSettings {
 
@@ -31,6 +34,14 @@ object Global extends GlobalSettings {
 			case NonFatal(e) =>
 				Logger.error("Could not migrate database, starting application with unmigrated database...")
 				Logger.error(e.getMessage())
+		}
+	}
+
+	/** Handle exceptions */
+	override def onError(request: RequestHeader, ex: Throwable) = {
+		Future.successful {
+			implicit val notifications = List(TemplateNotification()(ex.getMessage()))
+			Results.InternalServerError(views.html.errorPage("An internal error has occured"))
 		}
 	}
 
