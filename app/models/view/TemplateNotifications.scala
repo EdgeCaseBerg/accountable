e041af0d17b6c2079ff4a59b07ac3fde1bd6f5a5
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-10-24 20:44:44 -0400

    Add msgArgs to TemplateNotifications
    Realized that in order to use the messages object and provide meaningful
    error messages for substitions, I need to take those as parameters to
    the TemplateNotifications.

--- app/models/view/TemplateNotifications.scala
+++ app/models/view/TemplateNotifications.scala
@@ -4,5 +4,6 @@ package models.view
  *  @note While Play does have the flash scope, you can't use that without redirecting. So this class exists as a general notification
  *  @param msgKey The key to a message stored in play's i18n Message object
  *  @param notificationType The type of notification this is, affects how the notification is rendered
+ *  @param msgArgs Arguments that will be passed to the Messages object that are needed for the given msgKey
  */
-case class TemplateNotification(val msgKey: String = "views.error", val notificationType: TemplateNotificationsType = ErrorNotification)
+case class TemplateNotification(val msgKey: String = "views.error", val notificationType: TemplateNotificationsType = ErrorNotification)(val msgArgs: Any*)
