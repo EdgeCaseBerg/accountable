3438fc8f42c3d489e3d43db0dcc2458579e5a4e1
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-10-24 20:32:24 -0400

    Add TemplateNotifications class
    Even though play has a the flash scope to pass arbitrary messages out to
    the template, I'm going to use this class to do most of that. Reason
    being that in order to use .flashing, you need to redirect and I don't
    always intend on redirecting like that when I might need to display an
    error or something to the user.

---
+++ app/models/view/TemplateNotifications.scala
@@ -0,0 +1,8 @@
+package models.view
+
+/** Holds notifications which should be rendered to templates
+ *  @note While Play does have the flash scope, you can't use that without redirecting. So this class exists as a general notification
+ *  @param msgKey The key to a message stored in play's i18n Message object
+ *  @param notificationType The type of notification this is, affects how the notification is rendered
+ */
+case class TemplateNotification(val msgKey: String = "views.error", val notificationType: TemplateNotificationsType = ErrorNotification)
