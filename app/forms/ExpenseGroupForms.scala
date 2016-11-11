dcdf3bbdffc07b4a55cf1744a6aec45752f4728c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-11-10 23:16:14 -0500

    Add ExpenseGroupForms object

---
+++ app/forms/ExpenseGroupForms.scala
@@ -0,0 +1,21 @@
+package forms
+
+import models.domain.ExpenseGroup
+
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation._
+
+/** Container object for forms relating to the expense groups
+ */
+object ExpenseGroupForms {
+
+	/** Form for creating an expense group.
+	 *  @note only maps name because groupId is auto-generated
+	 */
+	val createExpenseGroupForm = Form[ExpenseGroup](
+		mapping(
+			"name" -> nonEmptyText
+		)(name => ExpenseGroup(name = name))(expenseGroup => Option(expenseGroup.name))
+	)
+}
