2bacb4a648824443af31fe711734f8f0f3c41857
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-11-20 18:07:01 -0500

    Create helper function for constraints + html5
    When we apply form mappings that set min/max's for fields, it's slightly
    sad we don't automatically get HTML5 validation markup applied to the
    input's that play generates for us. So this helper function helps handle
    that a bit

--- app/models/view/ImplicitFieldConstructorProvider.scala
+++ app/models/view/ImplicitFieldConstructorProvider.scala
@@ -1,7 +1,57 @@
 package models.view
 
+import play.api.data.Field
+import play.api.data.validation.Constraints
+
 /** Overrides the default play field constructor when imported into a template */
 object ImplicitFieldConstructorProvider {
 	import views.html.helper.FieldConstructor
 	implicit val myFields = FieldConstructor(views.html.common.fieldConstructor.f)
+
+	/** Convert commonly found constraints into arguments
+	 *  For example, play's min/max settings will be converted into HTML constraints on an input text
+	 *  @param field	 The field whose constraints will be examined
+	 *  @param args Additional args to add to the map <i>after</i> the field's constraints are converted
+	 *  @note Commonly found constraints are those <a href="https://playframework.com/documentation/2.3.x/api/scala/index.html#play.api.data.validation.Constraints$">provided by play</a>
+	 *  @return a Map containing arguments that can be passed to input helpers
+	 */
+	def constraintsToArgs(field: Field, args: (Symbol, Any)*): Seq[(Symbol, Any)] = {
+		/** @note We have to assign these to val's because you'll get exceptions like 'stable identifier required, but play.api.data.validation.Constraints.nonEmpty.name.get found.'
+		 */
+		val nonEmptyName = Constraints.nonEmpty.name.get
+		val minName = Constraints.min(0).name.get
+		val minStrictName = Constraints.min(0, true).name.get
+		val maxName = Constraints.max(0).name.get
+		val maxStrictName = Constraints.max(0).name.get
+		val maxLengthName = Constraints.maxLength(0).name.get
+		val minLengthName = Constraints.minLength(0).name.get
+		val pattern = Constraints.pattern(".*".r).name.get
+
+		val helperForMinMaxes = { w: Seq[Any] =>
+			w.headOption.getOrElse(0)
+		}
+
+		field.constraints.map {
+			case (constraintName, wrappedArray) =>
+				constraintName match {
+					case `nonEmptyName` => Option(('minLength -> 1))
+					case `minName` | `minStrictName` => {
+						Option(('min -> helperForMinMaxes(wrappedArray)))
+					}
+					case `maxName` | `maxStrictName` => {
+						Option(('max -> helperForMinMaxes(wrappedArray)))
+					}
+					case `minLengthName` => {
+						Option(('minLength -> helperForMinMaxes(wrappedArray)))
+					}
+					case `maxLengthName` => {
+						Option(('maxLength -> helperForMinMaxes(wrappedArray)))
+					}
+					case `pattern` => {
+						Option(('pattern -> wrappedArray.headOption.getOrElse(".*")))
+					}
+					case _ => Option.empty[(Symbol, Any)]
+				}
+		}.filter(_.isDefined).map(_.get).++(args)
+	}
 }
