44a7941924d406821679271eb77a3bd78bb38857
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-11-27 12:44:48 -0500

    Change type for ChangeGroupForm
    Not going to have the group Id be optional / able to be set to nothing.

--- app/forms/ExpenseForms.scala
+++ app/forms/ExpenseForms.scala
@@ -43,13 +43,13 @@ object ExpenseForms {
 	/** Type Alias for submission for changing an expenses group
 	 *  @note Handy for form templates typing
 	 */
-	type ChangeGroupForm = (UUID, Option[UUID])
+	type ChangeGroupForm = (UUID, UUID)
 
-	/** Form for changing an expenses group, if no group is submitted the group is cleared and the expense becomes 'ungrouped' */
+	/** Form for changing an expenses group */
 	val changeGroupForm = Form[ChangeGroupForm](
 		tuple(
 			"expenseId" -> uuid,
-			"groupId" -> optional(uuid)
+			"groupId" -> uuid
 		)
 	)
 
